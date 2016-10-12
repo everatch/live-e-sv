feaaf6a5196d3281f737ea7422f62a587260643f
中山 怜央 <mr_monster2010@yahoo.co.jp>
2016-10-12 17:19:20 +0900

    add conf_default.php

---
+++ conf_default.php
@@ -0,0 +1,10 @@
+<?php
+	$_config = array();
+	
+	// ----------------------------  CONFIG DB  ----------------------------- //
+	$_config['db']['host'] = 'localhost';
+	$_config['db']['user'] = 'root';
+	$_config['db']['pw'] = 'password';
+	$_config['db']['charset'] = 'utf8';
+	$_config['db']['dbname'] = 'live_e_sv';
+?>
